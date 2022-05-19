b287eca8f7df28593b8a6b002b1efcaf8140de90
somayeh <s.ghanavati94@gmail.com>
2022-05-19 12:48:24 +0430

    change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var color = ['red', 'green', 'pink', 'blue']
-var btn = document.getElementById('#btn')
-const color = document.querySelector('.color')
+var color = ['red', 'green', 'pink', 'blue'];
+var btn = document.getElementById('#btn');
+var color = document.querySelector('.color');
 btn.addEventListener('click', function () {
-  const randomNumber = getRandomNumber()
-  document.body.style.backgroundColor = color[randomNumber]
-  color.textContent = color[randomNumber]
-})
+    var randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = color[randomNumber];
+    color.textContent = color[randomNumber];
+});
 function getRandomNumber() {
-  return Math.floor(Math.random() * color.length)
+    return Math.floor(Math.random() * color.length);
 }
