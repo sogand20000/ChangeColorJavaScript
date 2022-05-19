44f6757387f1e54cd3f1842753312aa93aea2901
somayeh <s.ghanavati94@gmail.com>
2022-05-19 13:05:45 +0430

    fix bag

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var color = ['red', 'green', 'pink', 'blue'];
-var btn = document.getElementById('#btn');
-var color = document.querySelector('.color');
+var colors = ['red', 'green', 'pink', 'blue']
+var btn = document.getElementById('btn')
+var color = document.querySelector('.color')
 btn.addEventListener('click', function () {
-    var randomNumber = getRandomNumber();
-    document.body.style.backgroundColor = color[randomNumber];
-    color.textContent = color[randomNumber];
-});
+  var randomNumber = getRandomNumber()
+  document.body.style.backgroundColor = colors[randomNumber]
+  color.textContent = colors[randomNumber]
+})
 function getRandomNumber() {
-    return Math.floor(Math.random() * color.length);
+  return Math.floor(Math.random() * colors.length)
 }
