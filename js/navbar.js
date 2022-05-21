859f2049efff10dbfa9853889513c1d7e81a4abe
somayeh <s.ghanavati94@gmail.com>
2022-05-21 11:20:36 +0430

    add navbar

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+const navtoggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+navtoggle.addEventListener('click', function () {
+  if (links.classList.contains('show-links')) {
+    links.classList.remove('show-links')
+  } else links.classList.add('show-links')
+})
+console.log(links)
