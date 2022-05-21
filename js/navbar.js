f66b5f268b3b9c35753334cf2f45e71f084fbbee
somayeh <s.ghanavati94@gmail.com>
2022-05-21 11:22:19 +0430

    chage navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,9 @@
 const navtoggle = document.querySelector('.nav-toggle')
 const links = document.querySelector('.links')
 navtoggle.addEventListener('click', function () {
-  if (links.classList.contains('show-links')) {
+  /*  if (links.classList.contains('show-links')) {
     links.classList.remove('show-links')
-  } else links.classList.add('show-links')
+  } else links.classList.add('show-links') */
+  links.classList.toggle('show-links')
 })
 console.log(links)
